360fa87441510ff3f0c000bc9a887c92d27a4b81
Vinaysanepalli <vinaysimha19@gmail.com>
2024-03-03 13:05:21 +0530

    addedprogram2

---
+++ python1 - Copy.py
@@ -0,0 +1,21 @@
+#age = int(input("enter the age:"))
+#if age < 28:
+#    print("you are minor.")
+#elif age >= 28 and age < 65:
+#print("you are major")
+
+num = int(input("enter the number:"))
+if num%2==0:
+  print("{0}is even".format(num))
+else:
+    print("{0}is odd".format(num))
+    
+# Define the numbers
+num1 = 10
+num2 = 800
+
+# Calculate the sum
+sum = num1 + num2
+
+# Print the result
+print("The sum is:", sum)
